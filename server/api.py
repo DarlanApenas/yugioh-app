4f6e445ff2527f05f0f7fccc7601ffcaf85e7cb6
DarlanApenas <darlan.alfeu@dcx.ufpb.br>
2025-05-13 13:04:36 -0300

    final changes

--- server/api.py
+++ server/api.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilitar CORS
+CORS(app)
 
 @app.route('/random-gif', methods=['GET'])
 def random_gif():
